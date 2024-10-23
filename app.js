4cd51fa7377a3f18bcfb4d9eb27ce328de5e6f48
root <root@DESKTOP-QP1DA4L>
2024-10-23 12:13:14 +0530

    Add remove todo feature

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ function addTodo() {
     }
 }
 
+function removeTodo(index) {
+    todos.splice(index, 1);//Remove the todo at the specified index
+    renderTodos();//Re-render the todo list
+}
+
 function renderTodos() {
     const list = document.getElementById('todo-list');
     list.innerHTML = '';
-    todos.forEach((todo) => {
+    todos.forEach((todo, index) => {
         const listItem = document.createElement('li');
         listItem.textContent = todo;
+
+        //create a remove button for each todo
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Delete';
+        removeBtn.style.marginLeft = '10px';
+        removeBtn.style.color = 'red';
+        removeBtn.onclick = function () {
+            removeTodo(index);
+        }
+        listItem.appendChild(removeBtn);
+
         list.appendChild(listItem);
     });
 }
